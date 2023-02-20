c9dc3c257b4e6618d65bc8a61f4039e9f714aba9
toma2023 <mrinmoyehimadrita@gmail.com>
2023-02-20 21:26:19 +0600

    more es6 added

---
+++ recap.js
@@ -0,0 +1,27 @@
+// 1.var let const
+//break up with var 
+// 2. default parameters
+function calculateSalary(salary, tax = 0.25, bonus = 0){
+    const remaining = salary - salary * tax;
+    const total = remaining + bonus;
+    return total;
+
+}
+// 3.template String
+const elementHTML = `
+<div>
+    <h3>Name:</h3>
+<p>Address: </p>
+<p>Salary: ${calculateSalary(10000, 0, 0)}</p>
+<p>Others: ${numbers[2]}</p>
+</div>
+`
+// 4.Arrow function
+const doubleIt = x => x *2;
+const calculateSalary2 = (salary, tax, bonus) =>salary - salary * tax + bonus;
+// 5.spread 
+const ages = [11, 13, 15, 14,  10, 16];
+const newAges = [...ages, 22, 24, 23];
+// 6.destructuring
+const { x, y, z , ...c} = {x:45, y:12, z:33, name:'sakib al hasan', salary: 4500000}
+const [a, b, ...r] = [12, 45, 21, 65, 98];
